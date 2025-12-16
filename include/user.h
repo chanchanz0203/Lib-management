2d6878adf8f98a961fc581f2a931041788bae6db
loannguyen <nloan6476@gmail.com>
2025-12-17 00:35:15 +0700

    update book management function, user management function, lookup and display function code

---
+++ include/user.h
@@ -0,0 +1,23 @@
+#ifndef USER_H
+#define USER_H
+
+#include "book.h"
+
+struct borrow
+{
+    int userId;
+    int bookId;
+};
+
+struct user
+{
+    int id;
+    char name[50];
+};
+
+void addUser();
+void editUser(int targetId);
+void deleteUser(int targetId);
+void displayUser();
+
+#endif
